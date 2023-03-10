f435aaf34e7d5f21d32ad391fafef5796dd23234
bhargavi35 <bhargavibanu3513@gmail.com>
2023-03-10 23:11:07 +0530

    chamged server

--- server.js
+++ server.js
@@ -1,15 +1,26 @@
-const jsonServer = require('json-server')
+// const jsonServer = require('json-server')
 
-const server = jsonServer.create()
+// const server = jsonServer.create()
 
-const router = jsonServer.router('db.json')
+// const router = jsonServer.router('db.json')
 
-const middlewares = jsonServer.defaults()
+// const middlewares = jsonServer.defaults()
 
 
-server.use(middlewares)
-server.use('/api', router)
-server.listen(process.env.PORT || 5000, () => {
-  console.log('JSON Server is running')
-})
+// server.use(middlewares)
+// server.use('/api', router)
+// server.listen(process.env.PORT || 5000, () => {
+//   console.log('JSON Server is running')
+// })
 
+// server.js
+const jsonServer = require("json-server");
+const server = jsonServer.create();
+const router = jsonServer.router("db.json");
+const middlewares = jsonServer.defaults();
+const port = process.env.PORT || 3000;
+server.use(middlewares);
+server.use(router);
+server.listen(port, () => {
+  console.log("JSON Server is running", port);
+});
